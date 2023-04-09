81a276787d1389418817b4f52e5d51c447911eda
Alper Karagöz <alperkaragoz@outlook.com>
2023-04-09 23:53:19 +0300

    Add project files.

---
+++ AspNetCoreEnvironments.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCoreEnvironments.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCoreEnvironments.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // www.alperkaragoz.com
+        // www.alpha.alperkaragoz.com
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //throw new Exception("Has Errors");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
